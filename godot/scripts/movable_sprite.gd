145eb1e30d85695dbaca818f45dd7cbc9ccc4034
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-09-16 09:22:01 -0400

    Add hurt animations to player and enemy

--- godot/scripts/movable_sprite.gd
+++ godot/scripts/movable_sprite.gd
@@ -1,20 +1,27 @@
 extends AnimatedSprite2D
 
 
-@onready var parent := self.get_parent() as CharacterBody2D
+@onready var parent := self.get_parent() as BaseCharacter
+
+
+func _ready() -> void:
+	if parent and parent is BaseCharacter:
+		parent.health_changed.connect(_on_parent_health_changed)
 
 
 # using process because I'm not affecting physics, only visuals
 func _process(_delta: float) -> void:
+	var hurting := self.animation == "hurt" and self.is_playing()
+	var attacking := self.animation == "attack" and self.is_playing()
+	
+	#prevents attacking animation from being interrupted
+	if attacking or hurting: 
+		return
+	
 	var moving := parent.velocity != Vector2.ZERO
 	
 	var walking := self.animation == "walk" and self.is_playing()
 	var idling := self.animation == "idle" and self.is_playing()
-	var attacking := self.animation == "attack" and self.is_playing()
-	
-	#prevents attacking animation from being interrupted
-	if attacking: 
-		return
 	
 	if not moving and not idling:
 		play("idle")
@@ -30,3 +37,8 @@ func _process(_delta: float) -> void:
 func _on_attack_hurtbox_body_entered(_body: Node2D) -> void:
 	play("attack")
 	pass # Replace with function body.
+
+
+func _on_parent_health_changed(_amount: float, negative: bool) -> void:
+	if negative and self.sprite_frames.has_animation("hurt"):
+		play("hurt")
