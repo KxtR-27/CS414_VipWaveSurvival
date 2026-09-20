fae55bfaca4aea483d48f59878ccf9267f52ac23
Alex Bratek <nbratek99@gmail.com>
2026-09-20 13:19:00 -0400

    Update file path in main menu script

--- godot/components/main_menu/main_menu.gd
+++ godot/components/main_menu/main_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var character_select_scene :=  preload("res://components/character_select_screen/character_select.tscn")
+@onready var character_select_scene : PackedScene =  preload("res://components/character_select_screen/character_select_screen.tscn")
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(character_select_scene)
